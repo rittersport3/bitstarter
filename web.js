0fb7d9a0e7cd71c943cf89cae91db4e5e56a3b0b
Pedro Pimenta <herrpfeffer@gmail.com>
2013-07-13 19:17:30 +0000

    changed index.html

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = new Buffer(200);
 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    buffer = readFileSync('index.html');    
+    var buffer = fs.readFileSync('index.html');    
     response.send(buffer.toString());
 
 var port = process.env.PORT || 5000;
